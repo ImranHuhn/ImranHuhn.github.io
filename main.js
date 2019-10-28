5b8e9031a3ed8d5a6749439de42a41745ae74148
ImranHuhn <huhnimran@gmail.com>
2019-10-27 20:08:20 -0400

    added toggle for mobile menu

---
+++ main.js
@@ -0,0 +1,8 @@
+function mobMenu() {
+    var x = document.querySelector(".menu-options");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
